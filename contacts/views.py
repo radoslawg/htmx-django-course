133e237dc04c95d4766e25d8824b0f893b97ca46
Radosław Grzanka <radoslawg@gmail.com>
2025-03-08 21:16:38 +0100

    Lesson 5 - HTMX Active Search

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 
 # Create your views here.
@@ -8,3 +9,13 @@ def index(request):
     contacts = request.user.contacts.all().order_by("-created_at")
     context = {"contacts": contacts}
     return render(request, "contacts.html", context)
+
+
+@login_required
+def search_contacts(request):
+    query = request.GET.get("search", "")
+    contacts = request.user.contacts.filter(
+        Q(name__icontains=query) | Q(email__icontains=query)
+    ).order_by("-created_at")
+    context = {"contacts": contacts}
+    return render(request, "partials/contact-list.html", context)
